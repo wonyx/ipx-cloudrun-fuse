aad56a56275f3d550e59d7ee3b012d7126af4698
Narimasa Sakurai <6940948+wonyx@users.noreply.github.com>
2023-09-16 14:34:51 +0900

    Init (#1)

---
+++ index.mjs
@@ -0,0 +1,10 @@
+import { createServer } from 'node:http';
+import { createIPX, createIPXMiddleware } from 'ipx';
+import { createApp, fromNodeMiddleware, toNodeListener } from 'h3';
+
+const ipx = createIPX({});
+const ipxMiddleware = createIPXMiddleware(ipx);
+
+const app = createApp().use('/', fromNodeMiddleware(ipxMiddleware));
+
+createServer(toNodeListener(app)).listen(process.env.PORT || 4060);
